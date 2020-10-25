7013b2c1d6a22ef894d28fa862e3563a76338ec6
Eesshhiikkaa <69863000+Eesshhiikkaa@users.noreply.github.com>
2020-10-25 12:47:25 +0530

    My function

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,30 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y){
-    x<<- y
+    x <<- y
     inv <<- NULL
   }
   get <- function() {x}
   setInverse <- function(inverse) {inv <<- inverse}
   getInverse <- function() {inv}
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-
+  
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   inv <- x$getInverse()
   if(!is.null(inv)) {
     message("Getting cashed data")
     return(inv)
-        ## Return a matrix that is the inverse of 'x'
   }
   mat <- x$get()
   inv <- solve(mat, ...)
